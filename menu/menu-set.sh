0e2c9a0c8cab974823566b5d3b368ead96cee04b
Sigit Greskap <138499530+greskap@users.noreply.github.com>
2023-09-15 01:08:03 +0800

    Update menu-set.sh

--- menu/menu-set.sh
+++ menu/menu-set.sh
@@ -52,7 +52,6 @@ echo -e "     ${BICyan}[${BIWhite}06${BICyan}] Set Auto Reboot   "
 echo -e "     ${BICyan}[${BIWhite}07${BICyan}] Restart All Service"
 echo -e "     ${BICyan}[${BIWhite}08${BICyan}] Change Banner"
 echo -e "     ${BICyan}[${BIWhite}09${BICyan}] Cek Bandwith"
-echo -e "     ${BICyan}[${BIWhite}10${BICyan}] Setting Auto Reboot"
 
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
@@ -69,7 +68,6 @@ case $opt in
 7) clear ; restart ; exit ;;
 8) clear ; nano /etc/issue.net ; exit ;; #ssh-vpn banner.conf
 9) clear ; bw ; exit ;;
-10) clear ; jam ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; menu-set ;;
