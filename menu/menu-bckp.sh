ac715dd881e1f68af7f2ba73c6a782e32af5b0de
Sigit Greskap <138499530+greskap@users.noreply.github.com>
2023-09-15 01:06:21 +0800

    Update menu-bckp.sh

--- menu/menu-bckp.sh
+++ menu/menu-bckp.sh
@@ -1,93 +1,78 @@
+#!/bin/bash
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+# // Exporting Language to UTF-8
+
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+
+
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lyellow='\e[93m'
+Lgreen='\e[92m'
+NC='\e[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+grenbo="\e[92;1m"
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+# Getting
+export CHATID="5491480146"
+export KEY="5893916269:AAFoRG0z9y0Rewi6K3bF6_momM9Wyom6BGE"
+export TIME="10"
+export URL="https://api.telegram.org/bot$KEY/sendMessage"
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear 
 clear
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
-blue='\033[0;34m'
-ungu='\033[0;35m'
-Green="\033[32m"
-Red="\033[31m"
-WhiteB="\e[5;37m"
-BlueCyan="\e[5;36m"
-Green_background="\033[42;37m"
-Red_background="\033[41;37m"
-Suffix="\033[0m"
+echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
+echo -e "       ${BIWhite}${UWhite}BACKUP DATA ${NC}"
+echo -e ""
+echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Backup VPS Data      "
+echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Restore VPS Data     "
 
+echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
+echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
 echo ""
-echo -e "$green███╗   ███╗███████╗███╗   ██╗██╗   ██╗    ██████╗  █████╗  ██████╗██╗  ██╗██╗   ██╗██████╗ $NC"
-echo -e "$green████╗ ████║██╔════╝████╗  ██║██║   ██║    ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║   ██║██╔══██╗$NC"
-echo -e "$green██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║    ██████╔╝███████║██║     █████╔╝ ██║   ██║██████╔╝$NC"
-echo -e "$green██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║    ██╔══██╗██╔══██║██║     ██╔═██╗ ██║   ██║██╔═══╝ $NC"
-echo -e "$green██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝    ██████╔╝██║  ██║╚██████╗██║  ██╗╚██████╔╝██║     $NC"
-echo -e "$green╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝     ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝     $NC"
-echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
-echo -e
-echo -e "${BlueCyan}              Menu Backup                   "
-echo -e
-echo -e "            GRESKAP"
-echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
-echo
-echo
-echo -e "${BlueCyan}Pilih Nomor: "
-echo
-echo "1).Backup"
-echo "2).Restore"
-echo "3).strt"
-echo "4).limit speed"
-echo "5).autobackup"
-echo
-echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
-echo
-echo -e "${BlueCyan}"
-read -p    "Pilih Nomor └╼>>>   " bro
-
-if [ $bro = 1 ] || [ $bro = 1 ]
-then
-figlet "backup" | lolcat
-backup
-echo "--------------------------------------------------------"
-echo
-echo "Terimakasih sudah menggunakan layanan script GRESKAP"
-echo
-echo "--------------------------------------------------------"
-fi
-
-if [ $bro = 2 ] || [ $bro = 2 ]
-then
-figlet "Restore" | lolcat
-restore
-echo "--------------------------------------------------------"
-echo
-echo "Terimakasih sudah menggunakan layanan script GRESKAP"
-echo
-echo "--------------------------------------------------------"
-fi
-if [ $bro = 3 ] || [ $bro = 3 ]
-then
-figlet "strt"
-strt
-echo "--------------------------------------------------------"
-echo
-echo "Terimakasih sudah menggunakan layanan script GRESKAP"
-echo
-echo "--------------------------------------------------------"
-fi
-if [ $bro = 4 ] || [ $bro = 4 ]
-then
-limitspeed
-echo "--------------------------------------------------------"
-echo
-echo "Terimakasih sudah menggunakan layanan script GRESKAP"
-echo
-echo "--------------------------------------------------------"
-fi
-
-if [ $bro = 5 ] || [ $bro = 5 ]
-then
-autobackup
-echo "--------------------------------------------------------"
-echo
-echo "Terimakasih sudah menggunakan layanan script GRESKAP"
-echo
-echo "--------------------------------------------------------"
-fi
+read -p " Select menu :  "  opt
+echo -e   ""
+case $opt in
+01 | 1) clear ; backup ;;
+02 | 2) clear ; restore ;;
+00 | 0) clear ; menu ;;
+*) clear ; menu-bckp ;;
+esac
